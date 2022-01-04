73bf99e78197e4240a4c7118e29b7572340a1fb9
hanskw4267 <hans.kurnia@westonrobot.com>
2022-01-04 15:21:29 +0800

    src: added auto reconnect function

--- src/mobile_base_ros.cpp
+++ src/mobile_base_ros.cpp
@@ -296,6 +296,14 @@ void MobileBaseRos::Run(double loop_hz) {
   loop_period_ = 1.0 / loop_hz;
   ros::Rate rate(loop_hz);
   while (ros::ok()) {
+    if (auto_reconnect_ && !robot_->SdkHasControlToken()) {
+      auto ret = robot_->RequestControl();
+      if (ret != HandshakeReturnCode::kControlAcquired) {
+        std::cout << "Failed to gain control token, error code: "
+                  << static_cast<int>(ret) << std::endl;
+      }
+    }
+    
     PublishRobotState();
     PublishSensorData();
     PublishWheelOdometry();
